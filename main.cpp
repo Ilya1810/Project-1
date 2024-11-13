2cabb5c12eb7645b5e3fb3e763a37d3daa358978
Ilya1810 <ilayl70245@gmail.com>
2024-11-13 18:59:24 +0500

    Написана функция для чтения строк из файла

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 string Function_Nomber_1()
 {
-    return 0;
+    vector<string> result_string;
+
+    ifstream in;
+    in.open("1.txt");
+    if(!in.is_open())
+    {
+        wcout << L"Ошибка открытия файла!" << endl;
+        exit;
+    }
+
+    string temporary;
+    while (!in.eof())
+    {
+        in >> temporary;
+        result_string.push_back(temporary);
+    }
+    return result_string;
 }
 
 void Function_Nomber_2()
