ae4f3be570cbf3c23599f5a86bc45d348d1b4138
CurassierNox <smgm021204@gmail.com>
2024-11-13 19:47:53 +0500

    Реализация функций вывода на экран и записи в фаил

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
 string Function_Nomber_1()
@@ -7,18 +9,40 @@ string Function_Nomber_1()
     return 0;
 }
 
-void Function_Nomber_2()
+void Function_Nomber_2(const vector<string> &lines)
 {
+    cout << "Содержимое вектора строк:" << endl;
+    for (size_t i = 0; i < lines.size(); i++) {
+        cout << "| " << i + 1 << " | " << lines[i] << " |" << endl;
+    }
+    cout << endl;
 
 }
 
-void Function_Nomber_3()
+void Function_Nomber_3(const vector<string> &lines, const string &filename)//(Вектор, Имя фаила в который надо записать)
 {
+    // Открываем файл на запись
+    ofstream outFile(filename);
+
+    // Проверяем, открылся ли файл успешно
+    if (!outFile.is_open()) {
+        cout << "Ошибка при открытии файла: " << filename << endl;
+        return;
+    }
+
+    // Записываем каждую строку вектора в файл
+    for (const string& line : lines) {
+        outFile << line << endl;
+    }
+    outFile.close();
+    cout << "Содержимое записано в файл " << filename << endl;
+}
 
 }
 int main()
 {
     Function_Nomber_1();
-    Function_Nomber_2();
-    Function_Nomber_3();
+    Function_Nomber_2(lines);
+    Function_Nomber_3(lines,"lines.txt");
+    return 0;
 }
