3b7cd7fb7eaab1d22878bde29f1c42b39125a6fb
Ilya1810 <ilayl70245@gmail.com>
2024-11-13 19:52:38 +0500

    Функция вынесена в отдельный файл

--- main.cpp
+++ main.cpp
@@ -2,31 +2,10 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include "Read_file.h"
 
 using namespace std;
 
-//Функция, считывающая данные из файла в вектор строк
-vector<string> Function_Nomber_1()
-{
-    vector<string> result_string;
-
-    ifstream in;
-    in.open("1.txt");
-    if(!in.is_open())
-    {
-        wcout << L"Ошибка открытия файла!" << endl;
-        exit;
-    }
-
-    string temporary;
-    while (!in.eof())
-    {
-        in >> temporary;
-        result_string.push_back(temporary);
-    }
-
-    return result_string;
-}
 
 void Function_Nomber_2()
 {
